1ec7286c04eb7dfb3c8d140bbd15c0fd530f67c8
김태효 <51405291+coomind@users.noreply.github.com>
2025-05-31 14:12:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1010,4 +1010,4 @@ if __name__ == '__main__':
         db.create_all()
         # init_sample_data()  # 샘플 데이터 초기화
         load_election_csv()  # CSV 데이터 로드
-    app.run(debug=True, host='0.0.0.0', port==5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
